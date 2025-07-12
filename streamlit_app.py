b69ec61db70fd5ce7063d4a625df97254dcd962c
SuperableMars90 <zcrock92@gmail.com>
2025-07-11 22:32:41 -0400

    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ else:
 
 if "responses" not in st.session_state:
     st.session_state.responses = {}
-display_name = st.secrets['map'][valid_user]
+display_name = st.secrets['map'][username]
 print(display_name)
 main_qid = "Player"
 main_q = poll_data[main_qid]
