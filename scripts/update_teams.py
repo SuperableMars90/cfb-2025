ebb85f7e1afd59bd152fac2a5f32f7ac473aa9df
SuperableMars90 <zcrock92@gmail.com>
2025-09-12 21:34:24 -0400

    Week 3 changes

--- scripts/update_teams.py
+++ scripts/update_teams.py
@@ -2,10 +2,6 @@ import pandas as pd
 import json
 from datetime import datetime
 from pprint import pprint
-logpath = "data/drop_add_log.json"
-mappath = "data/current_teams.json"
-log = json.load(open(logpath))
-teammap = json.load(open(mappath))
 
 # drop = 'Arkansas State'
 # add = 'Colorado'
@@ -14,7 +10,7 @@ teammap = json.load(open(mappath))
 # addclass = 'P5'
 # addconf = 'Big 12'
 
-def check_teamnum(teammap,create=False):
+def check_teamnum(teammap,create=False,print_num=False):
     if create:
         unique_teams_map = {i:[] for i in teammap}
     for player in teammap:
@@ -27,21 +23,33 @@ def check_teamnum(teammap,create=False):
             unique_teams_map[player]=teams
         if len(teams)!=26:
             print(f'Improper number of teams for: {player}')
+        else:
+            check = 0
+        if print_num:
+            print(len(teams))
     if create:
         return unique_teams_map
+    else:
+        return check
+    
     
 def check_drop(unique_teams_map,player,drop):
     check = 0
     if drop not in unique_teams_map[player]:
         check+=1
+    else:
+        print('Drop error')
     return check
 
 def check_add(unique_teams_map,owner,add):
     check = 0 
     for player in unique_teams_map:
         if player!=owner:
-            if add in uniqe_teams_map[player]:
+            if add in unique_teams_map[player]:
                 check+=1
+            else:
+                print('Add error')
+
     return check
 
 def drop_team(team,player,teammap):
@@ -49,24 +57,27 @@ def drop_team(team,player,teammap):
     passes = []
     for conference in teammap[player]:
         try:
-            teammap[player][conference].remove(drop)
+            teammap[player][conference].remove(team)
             drops.append(conference)
         except:
+            #print(conference)
+            #print(teammap[player][conference])
+            #print('------------')
             passes.append(conference)
     print(f'Dropped teams from {drops}')
     print(f'Team not found in {passes}')
     return teammap
 
-def add_team(team,player,teammap,teamconf):
+def add_team(add,owner,teammap,teamconf):
     g5ind = ['American','Conference USA','Mid-American','Mountain West','Sun Belt','G5 Flex']
     p4ind = ['ACC','Big 10','Big 12','SEC','P4 Flex']
     if teamconf in g5ind:
-        addclass = 'P5'
-    elif teamconf in p4ind:
         addclass = 'G5'
+    elif teamconf in p4ind:
+        addclass = 'P4'
     else:
         addclass = teamconf
-    if addclass == 'P5':
+    if addclass == 'P4':
         teammap[owner][teamconf].append(add)
         teammap[owner]['P4 Flex'].append(add)
     elif addclass == 'G5':
@@ -80,6 +91,7 @@ def add_team(team,player,teammap,teamconf):
         for conference in g5ind:
             teammap[owner][conference].append(add)
             teammap[owner]['G5 Flex'].append(add)
+    teammap[owner]['Wild Card'].append(add)
     else:
         print('Whoops, there was an error. Incorrect classification')
     return teammap
@@ -88,19 +100,17 @@ def rewrite_json(data,jsonfile_path):
     with open(jsonfile_path,'w+') as f:
         json.dump(data,f)
 
-def drop_add(drop,add,owner,addconf,teammap):
+def drop_add(drop,add,owner,addconf,teammap,mappath,log,logpath):
     unique_teams_map = check_teamnum(teammap,create=True)
     check = 0
     check+=check_drop(unique_teams_map,owner,drop)
     check+=check_add(unique_teams_map,owner,add)
     if check == 0:
-        teammap = drop_team(team,player,teammap)
+        teammap = drop_team(drop,owner,teammap)
         teammap = add_team(add,owner,teammap,addconf)
     else:
         print('Check error')
-        break
-
-    check = check_teamnum(teammap,create=False)
+    check += check_teamnum(teammap,create=False)
     if check == 0:
         rewrite_json(teammap,mappath)
         now = datetime.now()
