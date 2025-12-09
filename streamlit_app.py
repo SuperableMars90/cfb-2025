0d87d808e4372358551c5bceff4ebf393df8fd42
SuperableMars90 <zcrock92@gmail.com>
2025-12-08 20:48:59 -0500

    Fixed another location of bowl list

--- streamlit_app.py
+++ streamlit_app.py
@@ -111,7 +111,7 @@ with st.form("bowl_form"):
 if submitted:
     st.success("Submitted!")
 
-    bowls_sorted = load_bowl_data("bowls.json")
+    bowls_sorted = load_bowl_data("data/bowl_list.json")
     sheet = get_sheet("Bowl Picks")  # name of your Google Sheet
     header = ensure_header(sheet, bowls_sorted)
     append_response(sheet, answers, bowls_sorted, header)
